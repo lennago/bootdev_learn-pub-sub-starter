5fdee4a3ec7d16c9d9afbf89ebe6c1ee7d1aa4a6
lennago <lg.general@pm.me>
2026-08-19 00:53:26 +0200

    added publishing of pause/resume and moves

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -20,22 +20,34 @@ func main() {
 	}
 	defer conn.Close()
 	fmt.Println("Peril game client connected to RabbitMQ!")
+	pubCh, err := conn.Channel()
+	if err != nil {
+		log.Fatalf("couldn't create channel: %v", err)
+	}
 	username, err := gamelogic.ClientWelcome()
 	if err != nil {
 		log.Fatalf("couldn't get username: %v", err)
 	}
-	_, queue, err := pubsub.DeclareAndBind(
+	gs := gamelogic.NewGameState(username)
+	if err := pubsub.SubscribeJSON(
 		conn,
 		routing.ExchangePerilDirect,
-		routing.PauseKey+"."+username,
-		routing.PauseKey,
-		pubsub.SimpleQueueTransient,
-	)
-	if err != nil {
-		log.Fatalf("could not subscribe to pause: %v", err)
+		fmt.Sprintf("%s.%s", routing.PauseKey, gs.GetUsername()),
+		routing.PauseKey, pubsub.SimpleQueueTransient,
+		handlerPause(gs),
+	); err != nil {
+		log.Fatalf("couldn't subscripe to pause: %v", err)
 	}
-	fmt.Printf("Queue %v declared and bound!\n", queue.Name)
-	gs := gamelogic.NewGameState(username)
+	if err := pubsub.SubscribeJSON(
+		conn,
+		routing.ExchangePerilTopic,
+		fmt.Sprintf("%s.%s", routing.ArmyMovesPrefix, gs.GetUsername()),
+		fmt.Sprintf("%s.*", routing.ArmyMovesPrefix),
+		pubsub.SimpleQueueTransient,
+		handlerMove(gs),
+	); err != nil {
+		log.Fatalf("couldn't subscribe to army moves: %v", err)
+	}
 	for {
 		input := gamelogic.GetInput()
 		if len(input) == 0 {
@@ -48,11 +60,21 @@ func main() {
 				continue
 			}
 		case "move":
-			_, err := gs.CommandMove(input)
+			move, err := gs.CommandMove(input)
 			if err != nil {
 				fmt.Println(err)
 				continue
 			}
+			if err := pubsub.PublishJSON(
+				pubCh,
+				routing.ExchangePerilTopic,
+				fmt.Sprintf("%s.%s", routing.ArmyMovesPrefix, move.Player.Username),
+				move,
+			); err != nil {
+				fmt.Printf("error: %s\n", err)
+				continue
+			}
+			fmt.Printf("Moved %v units to %s\n", len(move.Units), move.ToLocation)
 		case "status":
 			gs.CommandStatus()
 		case "help":
---
+++ internal/pubsub/subscribe.go
@@ -0,0 +1,43 @@
+package pubsub
+
+import (
+	"encoding/json"
+	"fmt"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func SubscribeJSON[T any](
+	conn *amqp.Connection,
+	exchange, queueName, key string,
+	queueType SimpleQueueType,
+	handler func(T),
+) error {
+	ch, queue, err := DeclareAndBind(conn, exchange, queueName, key, queueType)
+	if err != nil {
+		return fmt.Errorf("couldn't declare and bind queue: %v", err)
+	}
+	msgs, err := ch.Consume(queue.Name, "", false, false, false, false, nil)
+	if err != nil {
+		return fmt.Errorf("couldn't consume messages: %v", err)
+	}
+	unmarshaller := func(data []byte) (T, error) {
+		var target T
+		err := json.Unmarshal(data, &target)
+		return target, err
+	}
+
+	go func() {
+		defer ch.Close()
+		for msg := range msgs {
+			target, err := unmarshaller(msg.Body)
+			if err != nil {
+				fmt.Printf("couldn't unmarshal message: %v\n", err)
+				continue
+			}
+			handler(target)
+			msg.Ack(false)
+		}
+	}()
+	return nil
+}
